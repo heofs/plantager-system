f9c979ffa8165a9a62b4a996883fca97d1b41b97
Henning Ofstad <ofstad10@gmail.com>
2020-05-11 13:31:46 +0200

    Fix sleeper function

--- env-controller/main.py
+++ env-controller/main.py
@@ -1,71 +1,75 @@
 #! /usr/bin/env python3
 # countasync.py
 
-import asyncio
 import time
-import threading
 
+from lights_cycle import run_plan
 from subscriber import Subscriber
 
 exchange_name = 'lights-cycle'
 
-lights_plan = [{
-    "date": "base",
-    "start": "06:00",
-    "stop": "21:00"
-}, {
-    "date": "04.05.2020",
-    "start": "06:00",
-    "stop": "21:00"
-}]
+import threading
+import asyncio
+import math
 
 
 class Controller(threading.Thread):
     def __init__(self):
         super(Controller, self).__init__()
-        self._has_update = False
         self.cycle = 1
+        self._has_changed = False
         self.event_loop = asyncio.get_event_loop()
 
     def run(self):
         self.task = self.event_loop.create_task(self.task_func())
         self.event_loop.run_forever()
 
-    def start_cycle(self):
-        self.task = self.event_loop.create_task(self.task_func())
+    async def sleeper(self, delay):
+        if (delay == 0 or delay < 0):
+            return
+
+        if (delay < 1):
+            await asyncio.sleep(delay)
+            return
+
+        for _ in range(math.ceil(self.cycle)):
+            await asyncio.sleep(1)
+            if (self._has_changed):
+                break
 
     async def task_func(self):
         while True:
             print('Working... ', self.cycle)
-            await asyncio.sleep(self.cycle)
+
+            await self.sleeper(self.cycle)
+            if (self._has_changed):
+                self._has_changed = False
+                continue
 
     def set_cycle(self, new_cycle):
         print("setting cycle to", new_cycle)
-        self.task.cancel()
         self.cycle = new_cycle
-        self.task = self.event_loop.create_task(self.task_func())
-        asyncio.ensure_future(self.task, loop=self.event_loop)
+        self._has_changed = True
 
 
 thread = Controller()
 thread.start()
+time.sleep(4)
+thread.set_cycle(0.1)
+time.sleep(1)
+thread.set_cycle(2)
 
-
-def callback(data):
-    print("retrieved data -> %r" % data)
-    # print(data['seconds'])
-    thread.set_cycle(data['seconds'])
-
-
-try:
-    sub = Subscriber(callback, exchange_name=exchange_name)
-    sub.start_consuming()
-
-except (KeyboardInterrupt):
-    sub.close_connection()
-    print("\nClosed connection.")
-
-# time.sleep(2)
-# thread.set_cycle(2)
 # time.sleep(1)
-# thread.set_cycle(1)
+# thread.set_cycle(lights_plan)
+
+# def callback(data):
+#     # print("retrieved data -> %r" % data)
+#     thread.set_cycle(lights_plan)
+
+# try:
+#     sub = Subscriber(callback, exchange_name=exchange_name)
+#     sub.start_consuming()
+
+# except (KeyboardInterrupt):
+#     sub.close_connection()
+#     print("\nClosed connection.")
