254db90ca58a441e0b766505954c741c46b9448d
Henning Ofstad <ofstad10@gmail.com>
2020-05-16 20:42:32 +0200

    Add sensor reader

--- env-monitoring/sensors/sht31.py
+++ env-monitoring/sensors/sht31.py
@@ -1,1 +1,30 @@
 #! /usr/bin/env python3
+
+import smbus
+import time
+
+# Get I2C bus
+bus = smbus.SMBus(1)
+
+while True:
+    # SHT31 address, 0x44(68)
+    # Send measurement command, 0x2C(44)
+    #		0x06(06)	High repeatability measurement
+    bus.write_i2c_block_data(0x44, 0x2C, [0x06])
+
+    time.sleep(0.5)
+
+    # SHT31 address, 0x44(68)
+    # Read data back from 0x00(00), 6 bytes
+    # Temp MSB, Temp LSB, Temp CRC, Humididty MSB, Humidity LSB, Humidity CRC
+    data = bus.read_i2c_block_data(0x44, 0x00, 6)
+
+    # Convert the data
+    temp = data[0] * 256 + data[1]
+    cTemp = -45 + (175 * temp / 65535.0)
+    humidity = 100 * (data[3] * 256 + data[4]) / 65535.0
+
+    # Output data to screen
+    print("Temperature in Celsius is : %.2f C" % cTemp)
+    print("Relative Humidity is : %.2f %%RH" % humidity)
+    time.sleep(5)
