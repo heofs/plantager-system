53f40a6f98ca2861e6135e45ef48853faa950381
Henning Ofstad <ofstad10@gmail.com>
2020-05-22 15:45:34 +0200

    Add postgres queries

--- env-monitoring/database.py
+++ env-monitoring/database.py
@@ -1,67 +1,77 @@
 #! /usr/bin/env python3
 
-import sqlite3
-import time
+import os
+import psycopg2
+import psycopg2.extras
+import json
 
-db_name = '../database/env_data.db'
-table_name = 'sensor_data'
-table_fields = """
-    sensor_id TEXT NOT NULL,
-    type TEXT,
-    location TEXT,
-    value REAL,
-    created_at INTEGER
-    """
+from dotenv import load_dotenv
+load_dotenv()
+
+db_name = os.getenv("POSTGRES_DB")
+db_user = os.getenv("POSTGRES_USER")
+db_pw = os.getenv("POSTGRES_PASSWORD")
+meta_table = 'sensor_metadata'
+data_table = 'sensor_data'
 
 
 class Database:
     def __init__(self):
-        self.conn = sqlite3.connect(db_name)
-        self.check_existing_db()
-
-    def check_existing_db(self):
-        c = self.conn.cursor()
-        c.execute(
-            f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}'"
-        )
-
-        if (not c.fetchall()):
-            x = self.conn.cursor()
-            x.execute(f"CREATE TABLE '{table_name}' ({table_fields})")
-            print(f"No table found, created table '{table_name}'.")
+        self.conn = psycopg2.connect(host="localhost",
+                                     dbname=db_name,
+                                     user=db_user,
+                                     password=db_pw)
 
     def close_connection(self):
         self.conn.close()
 
+    def get_sensor_list(self):
+        c = self.conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        c.execute(f"SELECT id, type, location FROM {meta_table};")
+
+        sensor_list = []
+        for sensor in c:
+            sensor_list.append({
+                "sensor_id": sensor[0],
+                "type": sensor[1],
+                "location": sensor[2],
+            })
+
+        return sensor_list
+
     def insert_data(self, data):
         sensor_id = data['sensor_id']
-        env_type = data['type']
         location = data['location']
         value = data['value']
-        created = int(time.time())
-        c = self.conn.cursor()
-        values = (
-            sensor_id,
-            env_type,
-            location,
-            value,
-            created,
-        )
-        c.execute(f"INSERT INTO '{table_name}' VALUES (?,?,?,?,?)", values)
-        self.conn.commit()
+        try:
+            c = self.conn.cursor()
+            values = (sensor_id, location, value)
+            c.execute(
+                f"INSERT INTO {data_table} (sensor_id, location, value) VALUES (%s,%s,%s);",
+                values)
+            self.conn.commit()
+            return data
+        except (Exception):
+            return False
 
 
 if __name__ == "__main__":
     db = Database()
+
+    sensors = db.get_sensor_list()
+    # print(sensors)
+    s0 = sensors[0]
+
     x = 1
-    target = 100
+    target = 25
     while x < target:
+        progress = float((x / target)) * 100.0
         db.insert_data({
-            "sensor_id": "23jj2",
-            "type": "°C",
-            "location": "room",
-            "value": x,
+            "sensor_id": s0['sensor_id'],
+            "location": s0['location'],
+            "value": progress,
         })
-        print(float((x / target)) * 100.0)
+        print(progress)
         x += 1
+
     db.close_connection()
