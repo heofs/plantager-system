672978373213af5026330886bd77e140a585581e
Henning Ofstad <ofstad10@gmail.com>
2020-05-22 15:53:16 +0200

    add class for sht31 sensor

--- env-monitoring/sensors/sht31.py
+++ env-monitoring/sensors/sht31.py
@@ -3,28 +3,33 @@
 import smbus
 import time
 
-# Get I2C bus
-bus = smbus.SMBus(1)
 
-while True:
-    # SHT31 address, 0x44(68)
-    # Send measurement command, 0x2C(44)
-    #		0x06(06)	High repeatability measurement
-    bus.write_i2c_block_data(0x44, 0x2C, [0x06])
+class SHT31(object):
+    def __init__(self):
+        # Get I2C bus
+        self.bus = smbus.SMBus(1)
 
-    time.sleep(0.5)
+    def read_sensor(self):
+        '''
+        Returns a dict with temperature in celcius and humidity in RH.\n
+        Example { "t": 22.4, "rh": 55.2 }
+        '''
+        # SHT31 address, 0x44(68)
+        # Send measurement command, 0x2C(44)
+        #		0x06(06)	High repeatability measurement
+        self.bus.write_i2c_block_data(0x44, 0x2C, [0x06])
 
-    # SHT31 address, 0x44(68)
-    # Read data back from 0x00(00), 6 bytes
-    # Temp MSB, Temp LSB, Temp CRC, Humididty MSB, Humidity LSB, Humidity CRC
-    data = bus.read_i2c_block_data(0x44, 0x00, 6)
+        time.sleep(0.5)
 
-    # Convert the data
-    temp = data[0] * 256 + data[1]
-    cTemp = -45 + (175 * temp / 65535.0)
-    humidity = 100 * (data[3] * 256 + data[4]) / 65535.0
+        # SHT31 address, 0x44(68)
+        # Read data back from 0x00(00), 6 bytes
+        # Temp MSB, Temp LSB, Temp CRC, Humididty MSB, Humidity LSB, Humidity CRC
+        data = self.bus.read_i2c_block_data(0x44, 0x00, 6)
 
-    # Output data to screen
-    print("Temperature in Celsius is : %.2f C" % cTemp)
-    print("Relative Humidity is : %.2f %%RH" % humidity)
-    time.sleep(5)
+        # Convert the data
+        temp = data[0] * 256 + data[1]
+        cTemp = -45 + (175 * temp / 65535.0)
+        humidity = 100 * (data[3] * 256 + data[4]) / 65535.0
+
+        return {"t": cTemp, "rh": humidity}
+
